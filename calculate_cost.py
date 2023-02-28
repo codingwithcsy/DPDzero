d4a6c2708748bef003e4ebebcc545ae05fe229b8
codingwithcsy <119589164+codingwithcsy@users.noreply.github.com>
2023-02-28 18:43:45 +0530

    Update calculate_cost.py

--- calculate_cost.py
+++ calculate_cost.py
@@ -9,20 +9,13 @@ with open('channel-cost.json') as f:
     channel_data = json.load(f)
 
 # Calculate the total cost per day for each channel
-total_cost = {}
-for channel, channel_usage in channel_data.items():
-    for date, usage in channel_usage.items():
-        if date not in total_cost:
-            total_cost[date] = {}
-        if channel not in total_cost[date]:
-            total_cost[date][channel] = 0
-        total_cost[date][channel] += usage * cost_data[channel]
-
-# Convert the total cost from paisa to rupees
-for date, date_cost in total_cost.items():
-    for channel, channel_cost in date_cost.items():
-        total_cost[date][channel] = channel_cost / 100
+total_cost = []
+for date, channel_usage in channel_data.items():
+    cost_per_day = {"date": date}
+    for channel, usage in channel_usage.items():
+        cost_per_day[channel] = round(usage * cost_data[channel] / 100, 2)
+    total_cost.append(cost_per_day)
 
 # Write the total cost to a file
 with open('total_cost.json', 'w') as f:
-    json.dump(total_cost, f, sort_keys=True, indent=4)
+    json.dump(total_cost, f, indent=4)
